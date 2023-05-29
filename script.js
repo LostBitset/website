0670fb941fc40f57370d0018da20f8920478358e
Landon <102832670+LostBitset@users.noreply.github.com>
2023-05-29 19:09:44 -0400

    some actual code now lol

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ var page_dyns = document.getElementsByClassName("dynpage");
 var date_dyns = document.getElementsByClassName("dyndate");
 var time_dyns = document.getElementsByClassName("dyntime");
 
+var pages = {
+	"100": function() {},
+};
+
+function setpage(page) {
+	pages[page.toString()]();
+	for (const el of page_dyns) {
+		el.innerText = page.toString();
+	}
+}
+
+setpage(100);
+console.log("script done");
+
