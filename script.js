b703135771a9313639c47a5384aac4b858ec6807
LostBitset <102832670+LostBitset@users.noreply.github.com>
2023-05-29 19:59:28 -0400

    ok

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ var page_dyns = document.getElementsByClassName("dynpage");
 var date_dyns = document.getElementsByClassName("dyndate");
 var time_dyns = document.getElementsByClassName("dyntime");
 var dynhead = document.getElementsByClassName("dynhead")[0];
+var dyncontent = document.getElementsByClassName("dyncontent")[0];
 
 function formathead(text) {
 	let pad = (41 - text.length);
-	let padl = pad - (pad / 2);
-	let padr = pad - padl;
+	let padr = pad - Math.floor(pad / 2);
+	let padl = pad - padr;
 	let paddingl = "\u00A0".repeat(padl);
 	let paddingr = "\u00A0".repeat(padr);
 	return `${paddingl}${text}${paddingr}`;
@@ -20,6 +21,11 @@ function sethead(text) {
 var pages = {
 	"100": function() {
 		sethead("Hi There!");
+		dyncontent.innerText = "\nWelcome to CEEFAX. Simply enter three digits on your keyboard to view a particular page. Some useful pages are listed below:\n\n100 - Home (where you are now)\n\n101 - About Me\n\n201 - Things I Have Written\n\n301 - My GitHub Page\n\n302 - CEEFAX on Wikipedia\n\nBe aware that only pages 100-199 will be displayed through the CEEFAX interface.";
+	},
+	"101": function() {
+		sethead("About Me");
+		dyncontent.innerText = "\nUnder construction.";
 	},
 };
 
@@ -56,6 +62,7 @@ function updatedatetime() {
 	}
 }
 
+updatedatetime();
 setInterval(updatedatetime, 500);
 
 setpage(100);
