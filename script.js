e230c17b37c7490382ae260069b07c2f0253504b
LostBitset <102832670+LostBitset@users.noreply.github.com>
2023-05-29 19:27:41 -0400

    nice, top row

--- script.js
+++ script.js
@@ -20,11 +20,22 @@ function formatdate(date) {
 	return `${dow} ${dom} ${month}`;
 }
 
+function formattime(date) {
+	let hour = date.toLocaleString("default", { hour: "2-digit", hour12: false });
+	let mins = date.toLocaleString("default", { minute: "2-digit" });
+	let secs = date.toLocaleString("default", { second: "2-digit" });
+	return `${hour}:${mins}/${secs}`;
+}
+
 function updatedatetime() {
 	let datetime = new Date();
 	let date = formatdate(datetime);
+	let time = formattime(datetime);
 	for (const el of date_dyns) {
 		el.innerText = date;
+	}
+	for (const el of time_dyns) {
+		el.innerText = time;
 	}
 }
 
