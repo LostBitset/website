83190686a99305ff297cfb9218312251091f91d6
LostBitset <102832670+LostBitset@users.noreply.github.com>
2023-05-29 19:05:21 -0400

    first script

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
+var page_dyns = document.getElementsByClassName("dynpage");
+var date_dyns = document.getElementsByClassName("dyndate");
+var time_dyns = document.getElementsByClassName("dyntime");
 
